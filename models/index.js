8c42ba6f6332bd56b0148c695ca6e09c3b47baf9
Taylor Weeks <taylorlweeks707@gmail.com>
2021-03-20 14:27:01 -0400

    working on models index.js

--- models/index.js
+++ models/index.js
@@ -5,8 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
